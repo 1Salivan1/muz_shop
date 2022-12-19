1c34f3ca74d85c44202b6783241cccd6ea7f2096
1Salivan1 <eugenysalivan@gmail.com>
2022-12-19 23:50:43 +0300

    Add shop

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Product.css';
 
 function Product(props) {
     return (
         <div className='product'>
             <img className='product__image' src={props.img}></img>
             <h3 className='product__title'>{props.title}</h3>
-            <h3 className='product__зкшсу'>{props.price}</h3>
-            <button>В корзину</button>
+            <h3 className='product__price'>{props.price}</h3>
+            <button className='product__button'>В корзину</button>
         </div>
     );
 }
--- src/pages/Instruments.jsx
+++ src/pages/Instruments.jsx
@@ -1,38 +1,45 @@
 import React from 'react';
+import './Instruments.css';
 import Product from '../components/Product'
 import guitar from '../img/guitar.png';
+import cart from '../img/cart.png';
 
 function Instruments(props) {
     return (
-        <div className='shop'>
-            <div className='shop__search'>
-                <input className='shop__search-input'></input>
-                <select name='shop__search-sort'>
-                    <option value="1">По релевантности</option>
-                    <option value="2">От дешевых к дорогим</option>
-                    <option value="3">От дорогих к дешевым</option>
-                </select>
-            </div>
-            <div className='shop__main'>
-                <div className='shop__main-category'>
-                    <ul className='shop__main-category-list'>
-                        <li className='shop__main-category-item'>Гитары</li>
-                        <li className='shop__main-category-item'>Скприпки</li>
-                        <li className='shop__main-category-item'>Саксофоны</li>
-                        <li className='shop__main-category-item'>Барабаны</li>
-                    </ul>
+            <div className='shop'>
+                <div className='container'>
+                    <div className='shop__search'>
+                        <form>
+                            <input className='shop__search-input' placeholder="Поиск"></input>
+                            <select name='shop__search-sort'>
+                                <option value="1">По релевантности</option>
+                                <option value="2">От дешевых к дорогим</option>
+                                <option value="3">От дорогих к дешевым</option>
+                            </select>
+                        </form>
+                        <img src={cart} className='shop__search-cart'></img>
+                    </div>
+                    <div className='shop__main'>
+                        <div className='shop__main-category'>
+                            <ul className='shop__main-category-list'>
+                                <li className='shop__main-category-item'>Гитары</li>
+                                <li className='shop__main-category-item'>Скприпки</li>
+                                <li className='shop__main-category-item'>Саксофоны</li>
+                                <li className='shop__main-category-item'>Барабаны</li>
+                            </ul>
+                        </div>
+                        <div className='shop__main-products'>
+                            <Product className='shop__main-products-card' img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
+                            <Product className='shop__main-products-card' img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
+                            <Product className='shop__main-products-card' img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
+                            <Product className='shop__main-products-card' img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
+                            <Product className='shop__main-products-card' img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
+                            <Product className='shop__main-products-card' img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
+                            <Product className='shop__main-products-card' img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
+                        </div>
+                    </div>
                 </div>
-                <div className='shop__main-products'>
-                    <Product img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
-                    <Product img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
-                    <Product img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
-                    <Product img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
-                    <Product img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
-                    <Product img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
-                    <Product img={guitar} title={"Гитара"} price={"3200 грн"}></Product>
-                </div>
             </div>
-        </div>
     );
 }
 
