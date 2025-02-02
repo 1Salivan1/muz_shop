4cf6a643ef9214782e475d0c9542eae8884b33f5
1Salivan1 <eugenysalivan@gmail.com>
2025-02-02 02:35:39 +0200

    deleted uneccessary page

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Shop from "../../pages/Shop/Shop";
-import Contacts from "../../pages/Contacts/Contacts";
 import Main from "../../pages/Main/Main";
 import PersonalOrder from "../../pages/PersonalOrder/PersonalOrder";
 import styles from "./Navigation.module.css";
@@ -20,16 +19,12 @@ function Navigation() {
           <Link to="/order" className={styles["header-nav-item"]}>
             Персональный заказ
           </Link>
-          <Link to="/contacts" className={styles["header-nav-item"]}>
-            Контакты
-          </Link>
         </div>
       </nav>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/instruments" element={<Shop />} />
         <Route path="/order" element={<PersonalOrder />} />
-        <Route path="/contacts" element={<Contacts />} />
       </Routes>
     </div>
   );
